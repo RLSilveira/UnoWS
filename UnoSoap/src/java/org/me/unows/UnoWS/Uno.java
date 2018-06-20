38b06b9d3385c34a1ad0c1f3c61746883f690eef
Rodrigo L. da Silveira <rodrigo.leao.silveira@outlook.com>
2018-06-19 22:29:55 -0300

    Migracao do T1 para WS

---
+++ UnoSoap/src/java/org/me/unows/UnoWS/Uno.java
@@ -0,0 +1,289 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.me.unows.UnoWS;
+
+import java.util.LinkedList;
+import java.util.Random;
+
+/**
+ *
+ * @author Rodrigo
+ */
+class Uno {
+
+    final int totalCartas = 108;
+    private int userId1 = 0, userId2 = 0;
+    LinkedList<Integer> cartasP1, cartasP2;
+    boolean next = true; // true -> p1
+    Random gerador;
+    int[] baralho;
+    int numCartas;
+    int cartaMesa;
+
+    Uno(int userId1, int userId2) {
+        this.userId1 = userId1;
+        this.userId2 = userId2;
+
+        gerador = new Random(userId1 + userId2);
+
+        // cria baralho
+        baralho = new int[totalCartas];
+        for (int i = 0; i < totalCartas; ++i) {
+            baralho[i] = i;
+        }
+
+        // embaralha cartas
+        for (int c = 0; c < totalCartas; ++c) {
+            int outra = gerador.nextInt(totalCartas);
+            int aux = baralho[c];
+            baralho[c] = baralho[outra];
+            baralho[outra] = aux;
+        }
+        for (int c = 0; c < totalCartas * totalCartas; c++) {
+            int c1 = gerador.nextInt(totalCartas);
+            int c2 = gerador.nextInt(totalCartas);
+            int aux = baralho[c1];
+            baralho[c1] = baralho[c2];
+            baralho[c2] = aux;
+        }
+        numCartas = totalCartas;
+
+        // dar as cartas
+        cartasP1 = new LinkedList<>();
+        cartasP2 = new LinkedList<>();
+        for (int i = 0; i < 7; i++) {
+            cartasP1.add(baralho[--numCartas]);
+            cartasP2.add(baralho[--numCartas]);
+        }
+
+        // carta da mesa
+        cartaMesa = baralho[--numCartas];
+
+    }
+
+    int getNextPlayer() {
+
+        if (next) {
+            return userId1;
+        } else {
+            return userId2;
+        }
+    }
+
+    String getCartaMesa() {
+        return cartaToString(cartaMesa);
+    }
+
+    int getNumCartasBaralho() {
+        return numCartas;
+    }
+
+    int getNumCartas(int idUsuario, boolean oponente) {
+
+        if (idUsuario == userId1) {
+            if (oponente) {
+                return cartasP2.size();
+            } else {
+                return cartasP1.size();
+            }
+        } else {
+            if (oponente) {
+                return cartasP1.size();
+            } else {
+                return cartasP2.size();
+            }
+        }
+    }
+
+
+    String cartaToString(int carta) {
+
+        if (carta < 0 || carta >= 108) return "ERROR";
+
+        int cor = carta / 25;
+        int aux = carta - (cor * 25);
+        int valor = (aux + 1) / 2;
+
+        String s1 = null, s2 = null;
+
+        if (valor <= 9) {
+            // numero
+            s1 = "" + valor;
+        } else {
+            // especial
+            switch (valor) {
+                case 10:
+                    s1 = "Pu";
+                    break;
+                case 11:
+                    s1 = "In";
+                    break;
+                case 12:
+                    s1 = "+2";
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        switch (cor) {
+            case 0: // azul
+                s2 = "Az";
+                break;
+            case 1: // amarelo
+                s2 = "Am";
+                break;
+            case 2: // verde
+                s2 = "Vd";
+                break;
+            case 3: // vermelho
+                s2 = "Vm";
+                break;
+            case 4: //coringa
+                if (carta < 104) {
+                    s1 = "Cg";
+                } else {
+                    s1 = "C4";
+                }
+                s2 = "*";
+                break;
+        }
+
+        return s1 + "/" + s2;
+    }
+
+    public String getCartas(int idIsuario) {
+        LinkedList<Integer> cartas = null;
+        if (idIsuario == userId1) {
+            cartas = cartasP1;
+        } else if (idIsuario == userId2) {
+            cartas = cartasP2;
+        }
+
+        if (cartas == null || cartas.isEmpty()) {
+            return null;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        boolean b = false;
+        for (int i : cartas) {
+            if (b) sb.append("|");
+            else b = true;
+            sb.append(cartaToString(i));
+        }
+
+        return sb.toString();
+
+    }
+
+    public int getCor() {
+        int cor = cartaMesa / 25;
+        return cor;
+    }
+
+    public int compraCarta(int idIsuario) {
+        if (numCartas > 0) {
+            if (idIsuario == userId1) {
+                cartasP1.add(baralho[--numCartas]);
+                return 0;
+            } else if (idIsuario == userId2) {
+                cartasP2.add(baralho[--numCartas]);
+                return 0;
+            }
+        }
+
+        return -1;
+
+    }
+
+
+    public int passaVez(int idIsuario) {
+        next = !next;
+        return 0;
+    }
+
+    public int getPontosJogador(int idIsuario, boolean oponente) {
+
+        LinkedList<Integer> cartas = null;
+        if (idIsuario == userId1) {
+            if (oponente) {
+                cartas = cartasP2;
+            } else {
+                cartas = cartasP1;
+            }
+        } else if (idIsuario == userId2) {
+            if (oponente) {
+                cartas = cartasP1;
+            } else {
+                cartas = cartasP2;
+            }
+        }
+
+        if (cartas == null) return Integer.MAX_VALUE;
+
+        int p = 0;
+        for (int i : cartas) {
+            p += getCartaValue(i);
+        }
+
+        return p;
+    }
+
+    int getCartaValue(int carta) {
+
+        if (carta < 0 || carta >= 108) return Integer.MAX_VALUE;
+
+        int cor = carta / 25;
+        int aux = carta - (cor * 25);
+        int valor = (aux + 1) / 2;
+
+
+        if (cor > 3) return 50; // coringa
+
+        if (valor >= 10) return 20; // cartas de ação
+
+        return valor;
+
+    }
+
+
+    boolean isWO = false;
+    int campeao = 0;
+
+    public int encerraPartida(int idUsuario) {
+
+        isWO = true;
+        campeao = idUsuario == userId1 ? userId2 : userId1;
+        return 0;
+
+    }
+
+    public int jogaCarta(int idIsuario, int idxCarta, int cor) {
+
+        //  0 (jogada inválida: por exemplo, a carta não corresponde à cor que está na mesa)
+
+        if (!(next && idIsuario == userId1 || !next && idIsuario == userId2)){
+            return -4; // ­4 (não é a vez do jogador).
+        }
+
+        LinkedList<Integer> cartas = next ? cartasP1 : cartasP2;
+
+        if (idxCarta < 0 || idxCarta >= cartas.size()){
+            return -3; // ­3 (parâmetros inválidos)
+        }
+
+        cartaMesa = cartas.get(idxCarta);
+        cartas.remove(idxCarta);
+        next = !next;
+
+        if (cartaToString(cartaMesa).toUpperCase().startsWith("C")){
+            return -5; // coringa
+        }
+
+        return 1; //  1 (tudo certo)
+
+    }
+}
--- UnoSoap/src/java/org/me/unows/UnoWS/UnoWS.java
+++ UnoSoap/src/java/org/me/unows/UnoWS/UnoWS.java
@@ -5,6 +5,9 @@
  */
 package org.me.unows.UnoWS;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Random;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -16,11 +19,347 @@ import javax.jws.WebParam;
 @WebService(serviceName = "UnoWS")
 public class UnoWS {
 
-    /**
-     * This is a sample web service operation
-     */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
+    private static final int MATCH_CAPACITY = 500;
+
+    enum MatchState {
+        WaitStart,
+        InProgress,
+        Finished
     }
+
+    class Match {
+
+        int userId1 = 0, userId2 = 0;
+        MatchState state;
+        Uno game;
+        Date lastTime;
+
+        Match() {
+            state = MatchState.WaitStart;
+        }
+
+        int getUserId1() {
+            return userId1;
+        }
+
+        void setUserId1(int userId1) {
+            this.userId1 = userId1;
+        }
+
+        int getUserId2() {
+            return userId2;
+        }
+
+        void setUserId2(int userId2) {
+
+            this.userId2 = userId2;
+
+            // Start the match
+            game = new Uno(userId1, userId2);
+            state = MatchState.InProgress;
+
+        }
+
+    }
+    
+    
+
+    private final HashMap<Integer, String> dictUserIdNick;
+    private HashMap<Integer, Integer> dictUserIdMatche;
+    private final Match[] matches;
+
+    public UnoWS() {
+
+        matches = new Match[MATCH_CAPACITY];
+        dictUserIdNick = new HashMap<>(MATCH_CAPACITY * 2);
+        dictUserIdMatche = new HashMap<>(MATCH_CAPACITY * 2);
+
+    }
+
+    @WebMethod(operationName = "registraJogador")
+    public int registraJogador(@WebParam(name = "Nickname") String nick) {
+        // TODO: Remover partidas encerradas ha 120 segundas (erro: if da data errado)
+        // remover partidas encerradas ha mais de 120 segundas
+        /*long now = new Date().getTime() - 120 * 1000;
+        for (int i = 0; i < MATCH_CAPACITY; i++) {
+            Match m = matches[i];
+            if (m != null && m.state == MatchState.Finished && m.lastTime.getTime() < now) {
+                matches[i] = null;
+                dictUserIdMatche.remove(m.userId1);
+                dictUserIdMatche.remove(m.userId2);
+                dictUserIdNick.remove(m.userId1);
+                dictUserIdNick.remove(m.userId2);
+            }
+        }*/
+
+        if (dictUserIdNick.containsValue(nick)) {
+            return -1; // Usuario ja cadastrado
+        }
+
+        for (int i = 0; i < MATCH_CAPACITY; i++) {
+            Match match = matches[i];
+            if (match == null || match.state == MatchState.WaitStart) {
+
+                Random r = new Random();
+                int id = r.nextInt();
+                while (id <= 0 || dictUserIdNick.containsKey(id)) { // Id must be great than zero
+                    id = r.nextInt();
+                }
+
+                dictUserIdNick.put(id, nick);
+                dictUserIdMatche.put(id, i);
+
+                if (match == null) {
+                    match = new Match();
+                    matches[i] = match;
+                    match.setUserId1(id);
+                    matches[i] = match;
+                } else {
+                    match.setUserId2(id);
+                }
+
+                System.out.println("Id " + id + " registrado para jogador " + nick);
+
+                // Success
+                return id;
+
+            }
+        }
+
+        return -2; // Server is full
+    }
+
+    @WebMethod(operationName = "encerraPartida")
+    public int encerraPartida(@WebParam(name = "IdUsuario") int idUsuario) {
+        if (dictUserIdMatche.containsKey(idUsuario)) {
+            Match match = matches[dictUserIdMatche.get(idUsuario)];
+
+            if (match.state == MatchState.InProgress) {
+                match.state = MatchState.Finished;
+                match.lastTime = new Date();
+                return match.game.encerraPartida(idUsuario);
+            }
+        }
+
+        return -1; // erro
+    }
+
+    @WebMethod(operationName = "temPartida")
+    public int temPartida(@WebParam(name = "IdUsuario") int idUsuario) {
+        //TODO: temPartida return ­2 (tempo de espera esgotado)
+
+        if (dictUserIdMatche.containsKey(idUsuario)) { // verifica se ha partida para usuario
+            Match match = matches[dictUserIdMatche.get(idUsuario)];
+
+            if (match.state == MatchState.WaitStart) {
+
+                return 0; // ainda nao ha partida disponivel
+
+            } else if (match.state == MatchState.InProgress) {
+                if (idUsuario == match.getUserId1()) {
+                    return 1; // ha partida e jogador 1
+                } else {
+                    return 2; // ha partida e jogador 2
+                }
+            }
+
+        }
+
+        return -1;
+    }
+
+    @WebMethod(operationName = "obtemOponente")
+    public String obtemOponente(@WebParam(name = "IdIsuario") int idIsuario) {
+        int i = temPartida(idIsuario);
+        if (i > 0) {
+            Match match = matches[dictUserIdMatche.get(idIsuario)];
+            if (i == 1) { // sou usuario 1 entao retorna usuario 2
+                if (match.getUserId2() != 0) {
+                    return dictUserIdNick.get(match.getUserId2());
+                }
+            } else { // sou usuario e entao retorna usuario 1
+                if (match.getUserId1() != 0) {
+                    return dictUserIdNick.get(match.getUserId1());
+                }
+            }
+        }
+
+        return ""; // Erro
+
+    }
+
+    @WebMethod(operationName = "ehMinhaVez")
+    public int ehMinhaVez(@WebParam(name = "IdIsuario") int idIsuario) {
+        if (dictUserIdMatche.containsKey(idIsuario)) { // verifica se ha partida para usuario
+
+            Match match = matches[dictUserIdMatche.get(idIsuario)];
+            if (match.state == MatchState.WaitStart) {
+
+                return -2; // ainda nao ha partida disponivel
+
+            } else if (match.state == MatchState.InProgress) {
+
+                if (match.game.getNextPlayer() == idIsuario) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+
+            } else if (match.state == MatchState.Finished) {
+
+                if (match.game.campeao == 0) {
+                    return 4; // houve empate
+                }
+                if (match.game.campeao == idIsuario) {
+                    if (!match.game.isWO) {
+                        return 2; // é o vencedor 
+                    } else {
+                        return 5; // vencedor por WO 
+                    }
+                } else {
+                    if (!match.game.isWO) {
+                        return 3; // é o perdedor
+                    } else {
+                        return 6; // perdedor por WO
+                    }
+                }
+            }
+
+        }
+
+        return -1; // error
+    }
+
+    @WebMethod(operationName = "obtemNumCartasBaralho")
+    public int obtemNumCartasBaralho(@WebParam(name = "IdIsuario") int idIsuario) {
+        int r = temPartida(idIsuario);
+        if (r == 0) {
+            return -2; // nao ha dois jogadores
+        } else if (r > 0) {
+            Match match = matches[dictUserIdMatche.get(idIsuario)];
+            return match.game.getNumCartasBaralho();
+        } else {
+            return -1;
+        }
+    }
+
+    @WebMethod(operationName = "obtemNumCartas")
+    public int obtemNumCartas(@WebParam(name = "IdIsuario") int idIsuario) {
+        int r = temPartida(idIsuario);
+        if (r == 0) {
+            return -2; // nao ha dois jogadores
+        } else if (r > 0) {
+            Match match = matches[dictUserIdMatche.get(idIsuario)];
+            return match.game.getNumCartas(idIsuario, false);
+        } else {
+            return -1;
+        }
+
+    }
+
+    @WebMethod(operationName = "obtemNumCartarOponente")
+    public int obtemNumCartarOponente(@WebParam(name = "IdIsuario") int idIsuario) {
+        int r = temPartida(idIsuario);
+        if (r == 0) {
+            return -2; // nao ha dois jogadores
+        } else if (r > 0) {
+            Match match = matches[dictUserIdMatche.get(idIsuario)];
+            return match.game.getNumCartas(idIsuario, true);
+        } else {
+            return -1;
+        }
+    }
+
+    @WebMethod(operationName = "mostraMao")
+    public String mostraMao(@WebParam(name = "IdIsuario") int idIsuario) {
+        if (dictUserIdMatche.containsKey(idIsuario)) {
+            Match match = matches[dictUserIdMatche.get(idIsuario)];
+            return match.game.getCartas(idIsuario);
+        }
+        return null;
+    }
+
+    @WebMethod(operationName = "obtemCartaMesa")
+    public String obtemCartaMesa(@WebParam(name = "IdIsuario") int idIsuario) {
+        if (dictUserIdMatche.containsKey(idIsuario)) {
+            Match match = matches[dictUserIdMatche.get(idIsuario)];
+            return match.game.getCartaMesa();
+        }
+        return null;
+    }
+
+    @WebMethod(operationName = "obtemCorAtiva")
+    public int obtemCorAtiva(@WebParam(name = "IdIsuario") int idIsuario) {
+        if (dictUserIdMatche.containsKey(idIsuario)) {
+            Match match = matches[dictUserIdMatche.get(idIsuario)];
+            return match.game.getCor();
+        }
+        return -1;
+    }
+
+    @WebMethod(operationName = "compraCarta")
+    public int compraCarta(@WebParam(name = "IdIsuario") int idIsuario) {
+        if (dictUserIdMatche.containsKey(idIsuario)) {
+            Match match = matches[dictUserIdMatche.get(idIsuario)];
+            return match.game.compraCarta(idIsuario);
+        }
+        return -1;
+    }
+
+    @WebMethod(operationName = "jogaCarta")
+    public int jogaCarta(@WebParam(name = "IdIsuario") int idIsuario, @WebParam(name = "IdxCarta") int idxCarta, @WebParam(name = "Cor") int cor) {
+        //TODO: int jogaCarta(int idIsuario, int idxCarta, int cor)
+
+        if (dictUserIdMatche.containsKey(idIsuario)) {
+            Match match = matches[dictUserIdMatche.get(idIsuario)];
+            if (match.state == MatchState.WaitStart) {
+                return -2; // ­2 (partida não iniciada: ainda não há dois jogadores registrados na partida)
+            } else {
+
+                //  1 (tudo certo), 
+                //  0 (jogada inválida: por exemplo, a carta não corresponde à cor que está na mesa)
+                // ­3 (parâmetros inválidos)
+                // ­4 (não é a vez do jogador).
+                return match.game.jogaCarta(idIsuario, idxCarta, cor);
+
+            }
+        }
+
+        return -1; // ­1 (jogador não encontrado)
+    }
+
+    @WebMethod(operationName = "obtemPontos")
+    public int obtemPontos(@WebParam(name = "IdIsuario") int idIsuario) {
+        return obtemPontosAux(idIsuario, false);
+    }
+
+    @WebMethod(operationName = "obtemPontosOponente")
+    public int obtemPontosOponente(@WebParam(name = "IdIsuario") int idIsuario) {
+        return obtemPontosAux(idIsuario, true);
+    }
+
+    @WebMethod(operationName = "passarVez")
+    public int passarVez(@WebParam(name = "IdUsuario") int idUsuario) {
+        if (dictUserIdMatche.containsKey(idUsuario)) {
+            Match match = matches[dictUserIdMatche.get(idUsuario)];
+            return match.game.passaVez(idUsuario);
+        }
+        return -1;
+    }
+
+    private int obtemPontosAux(int idIsuario, boolean oponente) {
+        if (dictUserIdMatche.containsKey(idIsuario)) {
+            Match match = matches[dictUserIdMatche.get(idIsuario)];
+            if (match.state == MatchState.Finished) {
+                return match.game.getPontosJogador(idIsuario, oponente);
+            } else if (match.state == MatchState.WaitStart) {
+                return -2; // partida ainda não iniciada
+            } else {
+                return -3; // partida não finalizada
+            }
+        }
+
+        return -1; // Jogador nao encontrado
+    }
+
 }
